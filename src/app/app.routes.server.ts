13a334dfb232b63f52b8a5d05e466dfc5ad84d66
JulioAlb453 <fuliobeto18@gmail.com>
2026-04-10 21:18:29 -0600

    change storate for render

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'buyer/colaborador/:id/menu',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Client,
   },
   {
     path: '**',
