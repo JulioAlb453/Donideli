412ef666c8f45b34c35248e399f2e6ec7271e695
JulioAlb453 <fuliobeto18@gmail.com>
2026-04-16 08:33:31 -0600

    modify administrator view layout

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -12,6 +12,7 @@ import { AdminProductsPageComponent } from './pages/products/admin-products-page
 import { AdminGlobalSalesPageComponent } from './pages/global-sales/admin-global-sales-page.component';
 import { AdminFutureUpdatesPageComponent } from './pages/future-updates/admin-future-updates-page.component';
 import { AdminChatPanelComponent } from './components/admin-chat-panel/admin-chat-panel.component';
+import { AdminSidebarComponent } from './components/admin-sidebar/admin-sidebar.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { AdminChatPanelComponent } from './components/admin-chat-panel/admin-cha
     AdminGlobalSalesPageComponent,
     AdminFutureUpdatesPageComponent,
     AdminChatPanelComponent,
+    AdminSidebarComponent,
   ],
   imports: [CommonModule, SharedModule, AdminRoutingModule],
   providers: [
